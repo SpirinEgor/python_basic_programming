5a6317ea7072a345b71c15d56b1d20d4722ecc09
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2020-03-15 03:36:19 +1200

    Update data_worker.js

--- assignment_2/client/data_worker.js
+++ assignment_2/client/data_worker.js
@@ -1,57 +1,45 @@
 $(document).ready(function () {
-    draw_table()
-});
+    draw_tree()
+})
 
-function draw_table() {
-    $('#table_body').html('');
-    $.getJSON('http://127.0.0.1:5000/get_all', function (data) {
-        $.each(data, function (key, val) {
-            let row = "";
-            $.each(val, function (key, val) {
-                row += '<td>' + val + '</td>';
-            });
-            $('#table_body').append('<tr>' + row + '</tr>');
-        });
-    });
+function subtree_to_html(subtree) {
+    if (typeof(subtree) != 'object')
+        return subtree.toString()
+    let res = ''
+    for (let key in subtree)
+        res += '<li>' + key + ': ' + subtree_to_html(subtree[key]) + '</li>'
+    return '<ul type="none">' + res + '</ul>'
 }
 
+function draw_tree() {
+    $('#tree').html('')
+    $.getJSON('http://127.0.0.1:5000/get_accounts', function (data) {
+        $.each(data, function (key, val) {
+            $('#tree').append(
+                '<ul type="none"><li>' +
+                    val['name'] + ' (' + val['screen_name'] + '): ' +
+                    subtree_to_html(val['profile']) +
+                '</li></ul>')
+        })
+    })
+}
 
 function processForm(e) {
-    if (e.preventDefault) e.preventDefault();
-    let user_info = $('#form').serializeArray().reduce(function (obj, item) {
-        obj[item.name] = item.value;
-        return obj;
-    }, {});
-    let promise = new Promise(function(resolve, reject) {
-        $.ajax({
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            url: "http://127.0.0.1:5000/new_user",
-            data: JSON.stringify(user_info),
-            dataType: "json",
-            success: function (data) {
-                resolve(data);
-            },
-            error: function (err) {
-                reject(err);
+    if (e.preventDefault) e.preventDefault()
+    let screen_name = $('#screen_name')[0].value
+    $.get('http://127.0.0.1:5000/add_account?screen_name=' + screen_name,
+        function (data) {
+            if (data == 'OK') {
+                draw_tree()
+            } else {
+                console.log("Can't add new account")
             }
-        });
-    });
-
-    promise.then(function (_) {
-        draw_table();
-    }).catch(function (err) {
-        console.log("can't add new user: " + err);
-    })
-
-    // request.always(function (data) {
-    //     draw_table();
-    // });
+        })
 }
 
-const form = document.getElementById('form');
+const form = document.getElementById('form')
 if (form.attachEvent) {
-    form.attachEvent("submit", processForm);
+    form.attachEvent("submit", processForm)
 } else {
-    form.addEventListener("submit", processForm);
+    form.addEventListener("submit", processForm)
 }

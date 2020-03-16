b908947af35e7b519a048d5c31e46b2f7599e624
Lunev Artem <artem.e.lunev2000@gmail.com>
2020-03-16 22:17:43 +0300

    task2

--- assignment_2/server/app_db.py
+++ assignment_2/server/app_db.py
@@ -1,8 +1,10 @@
 import json
 import sqlite3
+import requests
 
 from flask import Flask, g, request
 from flask_cors import CORS
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 CORS(app)
@@ -22,10 +24,25 @@ def init_db():
         db = get_db()
         cursor = db.cursor()
         cursor.executescript(
-            """CREATE TABLE IF NOT EXISTS Users
-               (id integer primary key, name text not null,
-               surname text not null, age integer)"""
+            """CREATE TABLE IF NOT EXISTS University
+               (id integer primary key, university text not null,
+               menuItem text not null, link text not null)"""
         )
+
+        spbu = requests.get('https://spbu.ru/news-events')
+        spbu_html = spbu.content
+        spbu_soup = BeautifulSoup(spbu_html, 'html.parser')
+        for item in spbu_soup.find_all('li', {"class": "menu-item"}):
+            cursor.execute("""INSERT INTO University (university, menuItem, link) VALUES(?,?,?);""",
+                   ("SPBU", item.a.text, item.a['href']))
+
+        msu = requests.get('https://www.msu.ru/')
+        msu_html = msu.content
+        msu_soup = BeautifulSoup(msu_html, 'html.parser')
+        ul = msu_soup.find_all('ul', {"class": "nav"})[0]
+        for item in ul.find_all('a'):
+            cursor.execute("""INSERT INTO University (university, menuItem, link) VALUES(?,?,?);""",
+                   ("MSU", item.text, item['href']))
         db.commit()
 
 
@@ -33,24 +50,10 @@ def init_db():
 def get_all():
     db_cursor = get_db().cursor()
     db_cursor.row_factory = sqlite3.Row
-    db_cursor.execute("SELECT * From Users")
+    db_cursor.execute("SELECT * From University")
     result = db_cursor.fetchall()
     json_result = json.dumps([dict(row) for row in result])
     return json_result
-
-
-@app.route('/new_user', methods=['POST'])
-def create_new_user():
-    user_json = request.get_json()
-    for key in ['name', 'surname', 'age']:
-        assert key in user_json, f'{key} not found in the request'
-    query = f"INSERT INTO Users (name, surname, age) VALUES ('{user_json['name']}', '{user_json['surname']}', {user_json['age']});"
-    db_conn = get_db()
-    db_conn.execute(query)
-    db_conn.commit()
-    db_conn.close()
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-
 
 @app.teardown_appcontext
 def close_connection(exception):

dab6f2e5d6dcd220cf291c84f7d357ea4e1fd324
Pogozhelskaya Vlada <pogozhelskaya@gmail.com>
2020-03-26 14:27:33 +0300

    Added task02

--- assignment_2/server/app_db.py
+++ assignment_2/server/app_db.py
@@ -1,13 +1,16 @@
 import json
 import sqlite3
+import requests
 
 from flask import Flask, g, request
 from flask_cors import CORS
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 CORS(app)
 
 DATABASE = 'my_database.sqlite'
+books = []
 
 
 def get_db():
@@ -21,11 +24,61 @@ def init_db():
     with app.app_context():
         db = get_db()
         cursor = db.cursor()
+        cursor.executescript("""DROP TABLE IF EXISTS Books""")
         cursor.executescript(
-            """CREATE TABLE IF NOT EXISTS Users
-               (id integer primary key, name text not null,
-               surname text not null, age integer)"""
+            """CREATE TABLE Books
+            (id integer primary key,
+            library text not null,
+            author text not null,
+            title text not null)"""
         )
+
+        db.commit()
+
+
+def add_aldebaran():
+
+    page = requests.get("https://aldebaran.ru/genre/klassika/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    book_list = soup.find_all('div', {"class": "book_info clearfix"})
+    for book in book_list:
+        title = book.find('p', class_="booktitle").a.text
+        info = book.find('div', class_="book_intro").text
+        book_info = {
+            "library": "aldebaran.ru",
+            "title": title,
+            "info": info
+        }
+        books.append(book_info)
+
+
+def add_libs():
+    page = requests.get("https://libs.ru/best-100-russian/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    book_list = soup.find_all('div', class_="book-cardLong-info")
+    for book in book_list:
+        title = book.find(
+            'div', class_="book-cardLong-name").find('a').text
+        info = book.find(
+            'div', class_="book-cardLong-descr", recurcive=False).text
+        book_info = {
+            "library": "libs.ru",
+            "title": title,
+            "info": info
+        }
+        books.append(book_info)
+
+
+def fill_bd():
+    with app.app_context():
+        db = get_db()
+        cursor = db.cursor()
+
+        for book in books:
+            print(book["title"])
+            cursor.execute(f"""INSERT INTO Books
+                (library, title, author) VALUES
+                ('{book["library"]}', '{book["title"]}', '{book["info"]}');""")
         db.commit()
 
 
@@ -33,23 +86,10 @@ def init_db():
 def get_all():
     db_cursor = get_db().cursor()
     db_cursor.row_factory = sqlite3.Row
-    db_cursor.execute("SELECT * From Users")
+    db_cursor.execute("SELECT * From Books")
     result = db_cursor.fetchall()
     json_result = json.dumps([dict(row) for row in result])
     return json_result
-
-
-@app.route('/new_user', methods=['POST'])
-def create_new_user():
-    user_json = request.get_json()
-    for key in ['name', 'surname', 'age']:
-        assert key in user_json, f'{key} not found in the request'
-    query = f"INSERT INTO Users (name, surname, age) VALUES ('{user_json['name']}', '{user_json['surname']}', {user_json['age']});"
-    db_conn = get_db()
-    db_conn.execute(query)
-    db_conn.commit()
-    db_conn.close()
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @app.teardown_appcontext
@@ -61,4 +101,7 @@ def close_connection(exception):
 
 if __name__ == '__main__':
     init_db()
+    add_aldebaran()
+    add_libs()
+    fill_bd()
     app.run()

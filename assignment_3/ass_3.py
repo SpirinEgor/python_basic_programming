157f1303bd9a7a7ccfb98797137ae54215758439
Lunev Artem <artem.e.lunev2000@gmail.com>
2020-04-20 17:02:20 +0300

    fix

--- assignment_3/ass_3.py
+++ assignment_3/ass_3.py
@@ -1,9 +1,8 @@
 import numpy as np
 import cv2 as cv
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
-from flask import send_from_directory
 
 # from flask documentation
 UPLOAD_FOLDER = 'uploads'
@@ -54,32 +53,34 @@ def uploaded_file(filename):
 def colorize_image(filename):
     frame = cv.imread(app.config['UPLOAD_FOLDER'] + '/' + filename)
 
-    protoFile = "./models/colorization_deploy_v2.prototxt"
-    weightsFile = "./models/colorization_release_v2.caffemodel"
+    proto_file = "./models/colorization_deploy_v2.prototxt"
+    weights_file = "./models/colorization_release_v2.caffemodel"
 
     pts_in_hull = np.load('./pts_in_hull.npy')
 
-    net = cv.dnn.readNetFromCaffe(protoFile, weightsFile)
+    net = cv.dnn.readNetFromCaffe(proto_file, weights_file)
 
     pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1)
-    net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull.astype(np.float32)]
-    net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, np.float32)]
+    net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull
+                                                       .astype(np.float32)]
+    net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313],
+                                                          2.606, np.float32)]
 
-    W_in = 224
-    H_in = 224
+    weight_in = 224
+    height_in = 224
 
     img_rgb = (frame[:, :, [2, 1, 0]] * 1.0 / 255).astype(np.float32)
     img_lab = cv.cvtColor(img_rgb, cv.COLOR_RGB2Lab)
     img_l = img_lab[:, :, 0]
 
-    img_l_rs = cv.resize(img_l, (W_in, H_in))
+    img_l_rs = cv.resize(img_l, (weight_in, height_in))
     img_l_rs -= 50
 
     net.setInput(cv.dnn.blobFromImage(img_l_rs))
     ab_dec = net.forward()[0, :, :, :].transpose((1, 2, 0))
 
-    (H_orig, W_orig) = img_rgb.shape[:2]
-    ab_dec_us = cv.resize(ab_dec, (W_orig, H_orig))
+    (height_orig, weight_orig) = img_rgb.shape[:2]
+    ab_dec_us = cv.resize(ab_dec, (weight_orig, height_orig))
     img_lab_out = np.concatenate((img_l[:, :, np.newaxis], ab_dec_us), axis=2)
     img_bgr_out = np.clip(cv.cvtColor(img_lab_out, cv.COLOR_Lab2BGR), 0, 1)
 

b82a7a20c74ab877950e36662d2cf0ad5bd5fc6e
SergeyKuz1001 <rep78398@mail.ru>
2020-05-27 03:33:33 +1100

    Update human_pose_estimation.py and server.py

--- assignment_3/human_pose_estimation.py
+++ assignment_3/human_pose_estimation.py
@@ -49,29 +49,33 @@ models = {
     }
 }
 
+
 def pose_pairs(model):
     return models[model]["pose_pairs"]
+
 
 def key_points(image, model):
     if model not in ["COCO", "MPI"]:
         return None
 
-    protoFile = models[model]["proto_file"]
-    weightsFile = models[model]["weights_file"]
-    amountPoints = models[model]["amount_points"]
+    proto_file = models[model]["proto_file"]
+    weights_file = models[model]["weights_file"]
+    amount_points = models[model]["amount_points"]
 
-    imageWidth = image.shape[1]
-    imageHeight = image.shape[0]
+    image_width = image.shape[1]
+    image_height = image.shape[0]
     threshold = 0.1
 
-    net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+    net = cv2.dnn.readNetFromCaffe(proto_file, weights_file)
 
-    inWidth = 368
-    inHeight = 368
-    inpBlob = cv2.dnn.blobFromImage(image, 1.0 / 255, (inWidth, inHeight),
-                              (0, 0, 0), swapRB=False, crop=False)
+    in_width = 368
+    in_height = 368
+    inp_blob = cv2.dnn.blobFromImage(
+        image, 1.0 / 255, (in_width, in_height),
+        (0, 0, 0), swapRB=False, crop=False
+    )
 
-    net.setInput(inpBlob)
+    net.setInput(inp_blob)
 
     output = net.forward()
 
@@ -80,18 +84,19 @@ def key_points(image, model):
 
     points = []
 
-    for i in range(amountPoints):
-        probMap = output[0, i, :, :]
-        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-        x = (imageWidth * point[0]) / W
-        y = (imageHeight * point[1]) / H
+    for i in range(amount_points):
+        prob_map = output[0, i, :, :]
+        min_val, prob, min_loc, point = cv2.minMaxLoc(prob_map)
+        x = (image_width * point[0]) / W
+        y = (image_height * point[1]) / H
 
-        if prob > threshold : 
+        if prob > threshold:
             points.append((int(x), int(y)))
-        else :
+        else:
             points.append(None)
 
     return points
+
 
 def image_with_skeleton(initial_image, key_points, pose_pairs):
     image = np.copy(initial_image)
@@ -100,21 +105,34 @@ def image_with_skeleton(initial_image, key_points, pose_pairs):
         partA = pair[0]
         partB = pair[1]
         if key_points[partA] and key_points[partB]:
-            cv2.line(image, key_points[partA], key_points[partB], (0, 255, 255), 2)
-    
+            cv2.line(
+                image, key_points[partA], key_points[partB],
+                (0, 255, 255), 2
+            )
     for point in key_points:
-        if point != None:
-            cv2.circle(image, point, 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+        if point is not None:
+            cv2.circle(
+                image, point, 8, (0, 0, 255),
+                thickness=-1, lineType=cv2.FILLED
+            )
 
     return image
+
 
 def image_with_key_points(initial_image, key_points):
     image = np.copy(initial_image)
 
     for i in range(len(key_points)):
         point = key_points[i]
-        if point != None:
-            cv2.circle(image, point, 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(image, f"{i}", point, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+        if point is not None:
+            cv2.circle(
+                image, point, 8, (0, 255, 255),
+                thickness=-1, lineType=cv2.FILLED
+            )
+            cv2.putText(
+                image, f"{i}", point, cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 0, 255), 2, lineType=cv2.LINE_AA
+            )
 
     return image
+
--- assignment_3/server.py
+++ assignment_3/server.py
@@ -8,11 +8,13 @@ import human_pose_estimation as hpe
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main_page():
     return send_file("index.html")
 
-@app.route("/", methods = ["POST"])
+
+@app.route("/", methods=["POST"])
 def image_processing():
     image_storage = request.files["image"]
     image_buffer = image_storage.read()
@@ -24,10 +26,19 @@ def image_processing():
         key_points = hpe.key_points(image, model)
         pose_pairs = hpe.pose_pairs(model)
         image_with_key_points = hpe.image_with_key_points(image, key_points)
-        image_with_skeleton = hpe.image_with_skeleton(image, key_points, pose_pairs)
-        imwrite(f"images/{current_time}_image_with_key_points_{model}_model.jpeg", image_with_key_points)
-        imwrite(f"images/{current_time}_image_with_skeleton_{model}_model.jpeg", image_with_skeleton)
-    return render_template("result.html", time = current_time)
+        image_with_skeleton = hpe.image_with_skeleton(
+            image, key_points, pose_pairs
+        )
+        imwrite(
+            f"images/{current_time}_image_with_key_points_{model}_model.jpeg",
+            image_with_key_points
+        )
+        imwrite(
+            f"images/{current_time}_image_with_skeleton_{model}_model.jpeg",
+            image_with_skeleton
+        )
+    return render_template("result.html", time=current_time)
+
 
 @app.route("/images/<image_name>")
 def get_image(image_name):
@@ -36,6 +47,9 @@ def get_image(image_name):
 if __name__ == "__main__":
     if not os.path.exists("images/"):
         os.system("mkdir images/")
-    if not (os.path.exists("pose/coco/pose_iter_440000.caffemodel") and os.path.exists("pose/mpi/pose_iter_160000.caffemodel")):
+    if not (
+            os.path.exists("pose/coco/pose_iter_440000.caffemodel") and
+            os.path.exists("pose/mpi/pose_iter_160000.caffemodel")):
         os.system("./getModels.sh")
     app.run()
+

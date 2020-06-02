8e313c6a3f7bb60f1406cbe01bcdb7784781600c
Duletov <duletov.dmitriy@gmail.com>
2020-06-02 17:45:54 +0300

    Fix

--- assignment_3/HW3/Server/server.py
+++ assignment_3/HW3/Server/server.py
@@ -1,75 +1,69 @@
-import requests
 import numpy as np
 import cv2 as cv
 import os.path
 
 
-from flask import Flask, g, redirect, request, url_for, send_from_directory
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
 
-classesFile = "coco.names"
-classes = None
-with open(classesFile, 'rt') as f:
+classes_file = "coco.names"
+with open(classes_file, 'rt') as f:
     classes = f.read().rstrip('\n').split('\n')
 
-confThreshold = 0.5
-nmsThreshold = 0.4
-inpWidth = 416
-inpHeight = 416
+conf_threshold = 0.5
+nms_threshold = 0.4
+inp_width = 416
+inp_height = 416
 
-modelConfiguration = "yolov3.cfg"
-modelWeights = "yolov3.weights"
-
-net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+model_configuration = "yolov3.cfg"
+model_weights = "yolov3.weights"
 
 
 def get_outputs_names(net):
-    layersNames = net.getLayerNames()
-    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    layers_names = net.getLayerNames()
+    return [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
-def draw_pred(classId, conf, left, top, right, bottom, frame):
+def draw_pred(class_id, conf, left, top, right, bottom, frame):
     cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
     label = '%.2f' % conf
     if classes:
-        assert(classId < len(classes))
-        label = '%s:%s' % (classes[classId], label)
-    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-    top = max(top, labelSize[1])
-    cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv.FILLED)
+        assert(class_id < len(classes))
+        label = '%s:%s' % (classes[class_id], label)
+    label_size, base_line = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    top = max(top, label_size[1])
+    cv.rectangle(frame, (left, top - round(1.5*label_size[1])), (left + round(1.5*label_size[0]), top + base_line), (255, 255, 255), cv.FILLED)
     cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
 
 
 def postprocess(frame, outs):
-    frameHeight = frame.shape[0]
-    frameWidth = frame.shape[1]
+    frame_height = frame.shape[0]
+    frame_width = frame.shape[1]
 
-    classIds = []
+    class_ids = []
     confidences = []
     boxes = []
     for out in outs:
         for detection in out:
             scores = detection[5:]
-            classId = np.argmax(scores)
-            confidence = scores[classId]
-            if confidence > confThreshold:
-                center_x = int(detection[0] * frameWidth)
-                center_y = int(detection[1] * frameHeight)
-                width = int(detection[2] * frameWidth)
-                height = int(detection[3] * frameHeight)
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > conf_threshold:
+                center_x = int(detection[0] * frame_width)
+                center_y = int(detection[1] * frame_height)
+                width = int(detection[2] * frame_width)
+                height = int(detection[3] * frame_height)
                 left = int(center_x - width / 2)
                 top = int(center_y - height / 2)
-                classIds.append(classId)
+                class_ids.append(class_id)
                 confidences.append(float(confidence))
                 boxes.append([left, top, width, height])
 
-    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    indices = cv.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
     for i in indices:
         i = i[0]
         box = boxes[i]
@@ -77,25 +71,29 @@ def postprocess(frame, outs):
         top = box[1]
         width = box[2]
         height = box[3]
-        draw_pred(classIds[i], confidences[i], left, top, left + width, top + height, frame)
+        draw_pred(class_ids[i], confidences[i], left, top, left + width, top + height, frame)
 
 
 def detection(filename):
+    net = cv.dnn.readNetFromDarknet(model_configuration, model_weights)
+    net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+    net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+    
     if not os.path.isfile(filename):
         print("Input image file ", filename, " doesn't exist")
         sys.exit(1)
     cap = cv.VideoCapture(filename)
-    outputFile = filename[:-4]+'_detected.jpg'
+    output_file = filename[:-4]+'_detected.jpg'
 
     has_frame, frame = cap.read()
-    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+    blob = cv.dnn.blobFromImage(frame, 1/255, (inp_width, inp_height), [0, 0, 0], 1, crop=False)
     net.setInput(blob)
     outs = net.forward(get_outputs_names(net))
     postprocess(frame, outs)
     t, _ = net.getPerfProfile()
     label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
     cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-    cv.imwrite(outputFile, frame.astype(np.uint8))
+    cv.imwrite(output_file, frame.astype(np.uint8))
 
 
 @app.route('/image', methods=['POST'])
